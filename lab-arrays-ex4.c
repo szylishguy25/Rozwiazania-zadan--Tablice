46f8ffd27857bef1130a1d4472e65e31c01d0cfe
Szymon Wojciechowski <62310904+szylishguy25@users.noreply.github.com>
2020-03-31 14:20:43 +0200

    Update and rename lab3-4 to lab-arrays-ex4.c

--- lab-arrays-ex4.c
+++ lab-arrays-ex4.c
@@ -1,16 +1,26 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 int main()
 {
 	int i,j,l,m;
 	float tab[100],war=0,sr=0,br;
+	float r1,r2,wynik;
 	float min, max, k=0;
 	srand(time(NULL));
 	for (i=0; i<100; i++)
 	{
-		tab[i]=(1.0*rand()/RAND_MAX);
+		do
+		{
+			r1=(1.0*rand()/RAND_MAX);
+		    r2=(1.0*rand()/RAND_MAX);
+		    wynik=(cos(2*3.14*r2)*sqrt(-2.0*log(r1)));	//transformacja Boxa-Mullera
+		} while (wynik>=1.0 || wynik<=0.0);
+		tab[i]=wynik;
+		printf("Tab[%d]=%f\n",i+1,tab[i]);
 	}
 	min=tab[0];
 	max=tab[0];
@@ -29,7 +39,7 @@ int main()
 	}
 	printf("Najmniejsza liczba = %f\n",min);
 	printf("Największa liczba = %f\n",max);
-	printf("\n\nHistogram:\n\nLp	Wartosc tablicy			Histogram\n");
+	printf("\n\nHistogram:\n\nLp	Wartosc tablicy Histogram\n");
 		for (i=0; i<100; i++)
 	{
 		printf("%d	%f	",i+1,tab[i]);
